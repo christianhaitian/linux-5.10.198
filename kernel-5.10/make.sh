d5676fadc774df7308443a294f1cd2c62ee0e407
TheGammaSqueeze <thegammasqueeze@gmail.com>
2024-09-30 12:21:36 +0000

    Add bcmdhd as kernel module

--- kernel-5.10/make.sh
+++ kernel-5.10/make.sh
@@ -3,7 +3,8 @@ export CROSS_COMPILE=aarch64-linux-gnu- && make ARCH=arm64 x55_defconfig && make
 export CROSS_COMPILE=aarch64-linux-gnu- && make ARCH=arm64 x55_defconfig && make ARCH=arm64 KERNEL_CONFIG=x55_defconfig modules_install -j$(nproc)
 mkdir -p modules
 #cp drivers/media/usb/gspca/gspca_main.ko modules/
-#cp drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/bcmdhd.ko modules/
+cp drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/bcmdhd.ko modules/
+cp drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/dhd_static_buf.ko modules/
 #cp drivers/net/wireless/rockchip_wlan/rtl8188eu/8188eu.ko modules/
 #cp drivers/net/wireless/rockchip_wlan/rtl8188fu/8188fu.ko modules/
 #cp drivers/net/wireless/rockchip_wlan/rtl8189fs/8189fs.ko modules/
